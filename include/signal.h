kraj/uClibc

rework cancellation for sigwait, sigtimedwait and sigwaitinfo
sigtimedwait:
- provide __sigtimedwait_nocancel
- use __SYSCALL_SIGSET_T_SIZE instead of _NSIG / 8
- do not provide __sigtimedwait
- guard a section to avoid failure on archs if SI_TKILL/SI_USER are not defined
sigwaitinfo:
- simply use sigtimedwait since that handles cancellation already
sigwait:
- use non-cancellable functions (sigtimedwait, sigsuspend)
- get rid of code already done in __sigtimedwait_nocancel

Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>

include/signal.h
@@ -371,7 +371,8 @@ extern int sigtimedwait (__const sigset_t *__restrict __set,
 			 __const struct timespec *__restrict __timeout)
      __nonnull ((1));
 #ifdef _LIBC
-extern __typeof(sigtimedwait) __sigtimedwait attribute_hidden;
+extern __typeof(sigtimedwait) __sigtimedwait_nocancel attribute_hidden;
+libc_hidden_def(sigtimedwait)
 #endif
 
 /* Send signal SIG to the process PID.  Associate data in VAL with the

joel-porquet/tsar-uclibc

Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

include/signal.h
@@ -372,7 +372,8 @@ extern int sigtimedwait (__const sigset_t *__restrict __set,
 			 __const struct timespec *__restrict __timeout)
      __nonnull ((1));
 #ifdef _LIBC
-extern __typeof(sigtimedwait) __sigtimedwait attribute_hidden;
+extern __typeof(sigtimedwait) __sigtimedwait_nocancel attribute_hidden;
+libc_hidden_proto(sigtimedwait)
 #endif
 
 /* Send signal SIG to the process PID.  Associate data in VAL with the