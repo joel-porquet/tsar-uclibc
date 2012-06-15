kraj/uClibc

versionsort*.c, alphasort*.c: do not need *64 compiled for __WORDSIZE = 64
use a strong_alias instead

Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>

libc/misc/dirent/versionsort.c
@@ -12,3 +12,6 @@ int versionsort(const struct dirent **a, const struct dirent **b)
 {
 	return strverscmp((*a)->d_name, (*b)->d_name);
 }
+#if defined __UCLIBC_HAS_LFS__ && __WORDSIZE == 64
+strongs_alias_untyped(versionsort,versionsort64)
+#endif

joel-porquet/tsar-uclibc

Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

libc/misc/dirent/versionsort.c
@@ -12,3 +12,6 @@ int versionsort(const struct dirent **a, const struct dirent **b)
 {
 	return strverscmp((*a)->d_name, (*b)->d_name);
 }
+#if defined __UCLIBC_HAS_LFS__ && __WORDSIZE == 64
+strong_alias_untyped(versionsort,versionsort64)
+#endif