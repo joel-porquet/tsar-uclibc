kraj/uClibc

add LDSO_SAFE_RUNPATH config option
Signed-off-by: Peter S. Mazinger <ps.m@gmx.net>

ldso/ldso/dl-elf.c
@@ -174,8 +174,11 @@ search_for_named_library(const char *name, int secure, const char *path_list,
 				_dl_strcpy(mylibname, "."); /* Assume current dir if empty path */
 			_dl_strcat(mylibname, "/");
 			_dl_strcat(mylibname, name);
-			if ((tpnt = _dl_load_elf_shared_library(secure, rpnt, mylibname)) != NULL)
-				return tpnt;
+#ifdef __LDSO_SAFE_RUNPATH__
+			if (*mylibname == '/')
+#endif
+				if ((tpnt = _dl_load_elf_shared_library(secure, rpnt, mylibname)) != NULL)
+					return tpnt;
 			path_n = path+1;
 		}
 		path++;

joel-porquet/tsar-uclibc

Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

ldso/ldso/dl-elf.c
@@ -174,8 +174,11 @@ search_for_named_library(const char *name, unsigned rflags, const char *path_lis
 				_dl_strcpy(mylibname, "."); /* Assume current dir if empty path */
 			_dl_strcat(mylibname, "/");
 			_dl_strcat(mylibname, name);
-			if ((tpnt = _dl_load_elf_shared_library(rflags, rpnt, mylibname)) != NULL)
-				return tpnt;
+#ifdef __LDSO_SAFE_RUNPATH__
+			if (*mylibname == '/')
+#endif
+				if ((tpnt = _dl_load_elf_shared_library(rflags, rpnt, mylibname)) != NULL)
+					return tpnt;
 			path_n = path+1;
 		}
 		path++;