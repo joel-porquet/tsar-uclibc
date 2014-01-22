b3878612b974817e53d697b4e7604100c01b9850
Joel Porquet <joel.porquet@lip6.fr>
2014-01-22 13:37:12 +0100

    tsar: avoid error in including bits/sysnum.h
    the crt*.S should not include sysdep.h since it ends up including
    bits/sysnum.h which is not yet available when compiling them. Move asm
    macros to sys/asm.h instead.

--- libc/sysdeps/linux/tsar/sys/asm.h
+++ libc/sysdeps/linux/tsar/sys/asm.h
@@ -23,4 +23,33 @@
 #define SAVE_GP(x) \
 	.cprestore x
 
+#define _ENTRY(name)             \
+	.globl	name;            \
+	.align	2;               \
+	.type	name, @function; \
+	.ent	name, 0
+
+/* no frame */
+#define ENTRY(name)   \
+	_ENTRY(name); \
+	name:
+
+/* leaf routine */
+#define LEAF(name)    \
+	_ENTRY(name); \
+	name:	.frame sp, 0, ra
+
+/* nested routine */
+#define NESTED(name, framesz, rpc) \
+	_ENTRY(name);              \
+	name:	.frame sp, framesz, rpc
+
+#define	END(name)     \
+	.end	name; \
+	.size	name,.-name
+
+#define ret         \
+	jr	ra; \
+	nop
+
 #endif /* _SYS_TSAR_ASM_H */
--- libc/sysdeps/linux/tsar/sysdep.h
+++ libc/sysdeps/linux/tsar/sysdep.h
@@ -3,36 +3,8 @@
 
 #ifdef __ASSEMBLER__
 
+#include <sys/asm.h>
 #include <sys/regdef.h>
-
-#define _ENTRY(name)             \
-	.globl	name;            \
-	.align	2;               \
-	.type	name, @function; \
-	.ent	name, 0
-
-/* no frame */
-#define ENTRY(name)   \
-	_ENTRY(name); \
-	name:
-
-/* leaf routine */
-#define LEAF(name)    \
-	_ENTRY(name); \
-	name:	.frame sp, 0, ra
-
-/* nested routine */
-#define NESTED(name, framesz, rpc) \
-	_ENTRY(name);              \
-	name:	.frame sp, framesz, rpc
-
-#define	END(name)     \
-	.end	name; \
-	.size	name,.-name
-
-#define ret         \
-	jr	ra; \
-	nop
 
 #else /* ! __ASSEMBLER__ */
 
